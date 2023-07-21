8e943bb90e8ccc4ac3e77c2d6f87bd627197b648
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-22 03:11:44 +0600

    Project getting error to deploy

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({ title, link, img }: Props) => {
           width={350}
           height={350}
           src={img}
-          alt="Amazonclone"
+          alt="Graphic Design"
         />
         <p className="absolute hidden group-hover:inline-block bottom-0 w-full py-1 bg-blue-600 text-white text-center font-semibold duration-300">
           {title}
