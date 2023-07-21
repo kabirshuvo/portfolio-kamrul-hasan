9b9c8909fba0875e37675a63cb2dd30e322f0bdf
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-22 03:58:50 +0600

    Abigb link Added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import Link from "next/link";
+
+type Props = {
+  link: string;
+};
 
 const NavBar = () => {
   return (
     <div className="justify-center items-center text-center flex flex-col text-xl text-white font-bold pt-7">
       <span className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-        A Big B
+       <Link href={'https://www.abigb.com'} >A Big B</Link> 
       </span>
       <br />
       <span className="text-sm -mt-7">My Print on Demand Initiative</span>
