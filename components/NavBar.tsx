2da5c083e2d2102aa055ebfd1f299442b697828b
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-22 04:06:48 +0600

    Link should be open with a new tab

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <div className="justify-center items-center text-center flex flex-col text-xl text-white font-bold pt-7">
       <span className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-       <Link href={'https://www.abigb.com'} >ABigB.com</Link> 
+       <Link href={'https://www.abigb.com'} target="_blank">ABigB.com</Link> 
       </span>
       <br />
       <span className="text-sm -mt-7">My Print on Demand Initiative</span>
