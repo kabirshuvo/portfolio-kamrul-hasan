cee808339f0feb6ea7fe4017b0c480d52fb6bd4e
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-27 15:04:16 +0600

    skills section updated

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,50 +14,17 @@ const Skills = () => {
           title="Adobe Photoshop"
           link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
         />
-        <SkillsInput title="Photoshop" link="https://react.dev/" />
-        <SkillsInput title="Photoshop" link="https://nextjs.org/" />
-        <SkillsInput title="Photoshop" link="https://nodejs.org/en" />
+        <SkillsInput title="Adobe Illustrator" link="https://react.dev/" />
+        <SkillsInput title="Amazon Seller Center" link="https://nextjs.org/" />
+        <SkillsInput title="Amazon Advertising" link="https://nodejs.org/en" />
         <SkillsInput
-          title="Adobe Illustrator"
+          title="Print On Demand"
           link="https://www.typescriptlang.org/"
         />
-        <SkillsInput title="Illustrator" link="https://expressjs.com/" />
-        <SkillsInput title="Illustrator" link="" />
-        <SkillsInput title="Illustrator" link="https://www.mongodb.com/" />
-        <SkillsInput
-          title="Typography"
-          link="https://firebase.google.com/"
-        />
-        <SkillsInput title="Typesetting" link="https://tailwindcss.com/" />
-        <SkillsInput title="Sanity.io" link="https://www.sanity.io/" />
-        <SkillsInput
-          title="Design Principles"
-          link="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
-        />
-        <SkillsInput
-          title="Design Principles"
-          link="https://developer.mozilla.org/en-US/docs/Web/CSS"
-        />
-        {/* <SkillsInput title="VS Code" link="https://code.visualstudio.com/" /> */}
-        <SkillsInput
-          title="Atom"
-          link="https://github.blog/2022-06-08-sunsetting-atom/"
-        />
-        {/* <SkillsInput title="Git" link="https://git-scm.com/" />
-        <SkillsInput title="Github" link="https://github.com/" />
-        <SkillsInput title="Trello" link="https://trello.com/en" />
-        <SkillsInput title="GitKraken" link="https://www.gitkraken.com/" />
-        <SkillsInput title="UI Design" link="https://www.figma.com/" />
-        <SkillsInput title="Figma" link="https://www.figma.com/" />
-        <SkillsInput title="Canva" link="https://www.canva.com/" /> */}
-        <SkillsInput
-          title="Adobe Illustrator"
-          link="https://www.adobe.com/products/illustrator/free-trial-download.html"
-        />
-        {/* <SkillsInput title="Brand & LogoDesign" link="https://dribbble.com/" />
-        <SkillsInput title="Website Design" link="https://themeforest.net/" />
-        <SkillsInput title="Vercel" link="https://vercel.com/" />
-        <SkillsInput title="netlify" link="https://www.netlify.com/" /> */}
+      
+        <SkillsInput title="Content Creation" link="https://expressjs.com/" />
+        <SkillsInput title="MidJourney" link="" />
+       
       </div>
 
       <div>
